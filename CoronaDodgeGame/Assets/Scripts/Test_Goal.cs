e0cda761cc55321f55b3d801b7b37e3a4e658a82
YuuVR <leyuudachi@hotmail.com>
2020-04-24 23:38:17 +0200

    Uhm, stuff. xd

---
+++ CoronaDodgeGame/Assets/Scripts/Test_Goal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Goal : MonoBehaviour
+{
+
+    /*private void OnCollisionEnter(Collision col)
+    {
+        if (col.)
+        {
+
+        }
+    } */
+
+
+}
