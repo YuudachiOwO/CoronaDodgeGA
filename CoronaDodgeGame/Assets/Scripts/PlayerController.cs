33ab0ee7d259ae42868288a2cd8c4a54b6860ce6
YuuVR <leyuudachi@hotmail.com>
2020-04-25 12:44:57 +0200

    Adjusted movement, added stuff

--- CoronaDodgeGame/Assets/Scripts/PlayerController.cs
+++ CoronaDodgeGame/Assets/Scripts/PlayerController.cs
@@ -1,32 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
-
+    //Variables
     public float moveSpeed;
     private Rigidbody rb;
     private Vector3 moveInput;
     private Vector3 moveVelocity;
 
 
+
     void Start()
     {
+        //Get Component rigidbody of the player
+        //rb = GetComponent<Rigidbody>();
         rb = GetComponent<Rigidbody>();
     }
 
-
-    void Update()
+    private void Update()
     {
         moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
         moveVelocity = moveInput * moveSpeed;
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         rb.velocity = moveVelocity;
     }
+
+    void moveChar(Vector3 direction)
+    {
+        rb.MovePosition((Vector3)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+
+
 
 
 }
